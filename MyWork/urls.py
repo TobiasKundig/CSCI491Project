a225df224b07c1f46bee5b76568484743a3362bd
Matthew Dolan <mdolan2424@gmail.com>
2016-10-24 15:59:39 -0600

    RegisterView and LoginView

--- MyWork/urls.py
+++ MyWork/urls.py
@@ -1,11 +1,13 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^portfolio/', include('portfolio.urls')),
-    url(r'^$', TemplateView.as_view(template_name='static/index.html'),name='home'),
+    url(r'^$', include('portfolio.urls'))
+
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.http import JsonResponse
 
+from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http import HttpResponse
 
@@ -9,49 +10,97 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import User
 from .SQLFacade import *
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect
+from django.views.generic import View
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+
+
+#forms
+from .forms import User_Registration
+
 #always pass in a request and return a response
 def index(request):
+     #check if logged in
+
+
      all_users = User.objects.all()
 
      return render(request, 'portfolio/index.html', {'all_users': all_users})
    # return render(request,'portfolio/index.html')
 
+def profile(request):
+    profile = get_object_or_404(User, username=request.user)
+
+
+
+    return render(request, 'portfolio/portfolio.html', {'this_account': profile})
 #present the details of a portfolio.
-def detail(request, account):
+def detail(request, user):
     #if not request.user.is_authenticated():
         #return render(request, 'portfolio/login.html')
     #else:
 
+    #profile object = Model, where username=user
+    profile = get_object_or_404(User,username=user)
 
-    account = get_object_or_404(Account, username=account)
-    #portfolio = get_object_or_404(Portfolio, portfolio=account)
-    return render(request, 'portfolio/portfolio.html', {'this_account': account})
+    #passback the object. The template will ask for properties.
+    return render(request, 'portfolio/portfolio.html', {'this_account': profile})
 
+class RegisterView(View):
+    def get(self, request, *args, **kwargs):
+        form = User_Registration()
+        return render(request, 'portfolio/register.html', {'form': form})
 
-def login(request):
+    def post(self, request, *args, **kwargs):
 
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
 
-    return
+        form = User_Registration(request.POST)
 
-def logout(request):
+        if form.is_valid():
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
 
+            return redirect('login')
+        return render(request, 'portfolio/register.html', {'form': form})
+class ProfileView(View):
+    def get(self, request, *args, **kwargs):
+        return None
+    def post(self, request, *args, **kwargs):
+        return None
 
-    return
-def authenticate(request):
+class LoginView(View):
 
+    template_name = ['portfolio/login.html', 'portfolio/portfolio.html']
 
-    return
+    def get(self, request, *args, **kwargs):
 
-def signup(request):
+        if request.user.is_authenticated():
 
-    return
+            return redirect('profile')
+        return render(request, self.template_name[0])
 
+    def post(self, request, *args, **kwargs):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if user.is_active:
+                return redirect('profile')
+        else:
+            messages.add_message(
+                request, messages.ERROR, "Invalid username or password"
+            )
+            return HttpResponseRedirect(reverse('portfolio:login'))
 
-#class based views. I (Matt) haven't worked much with them yet.
-class portfolio(View):
-    portfolio = "myportfolio"
-
-    def get(self, request):
-
-        return HttpResponse(self.portfolio)
+def LogoutView(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('portfolio:index'))
 
