bff8bc5705b667a42f6be839274f2c16e768196b
chloeqrussell <chloe.quinn.russell@gmail.com>
2016-11-15 16:30:10 -0700

    added contact page and fixed minor css problems

--- MyWork/urls.py
+++ MyWork/urls.py
@@ -1,4 +1,6 @@
 
+
+
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -8,5 +10,5 @@ urlpatterns = [
 
     	url(r'^admin/', include(admin.site.urls)),
     	url(r'^', include('portfolio.urls'))
-
+        
 ]
